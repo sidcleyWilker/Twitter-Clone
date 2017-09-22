910be3c7c6cae26a17fa19a1f6b086a3d712618f
sidcleywilker <sidcleywilker@gmail.com>
2017-09-22 00:55:02 -0300

    Controlando a sessao do usuario

---
+++ home.php
@@ -0,0 +1,76 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        header('Location: index.php?erro=1');
+    }
+?>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>Twitter clone</title>
+		
+		<!-- jquery - link cdn -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+		<!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+	</head>
+
+	<body>
+
+		<!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <img src="imagens/icone_twitter.png" />
+	        </div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="sair.php">Sair</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+
+
+	    <div class="container">
+	    	
+	    	<br /><br />
+
+	    	<div class="col-md-4"></div>
+	    	<div class="col-md-4">
+	    		<h1>Usuario autenticado</h1>
+	    		<br>
+	    		<?= $_SESSION['usuario'] ?>
+	    		<br>
+	    		<?= $_SESSION['email'] ?>
+	    	</div>
+			<div class="col-md-4"></div>
+
+			<div class="clearfix"></div>
+			<br />
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+
+		</div>
+
+
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
--- validar_acesso.php
+++ validar_acesso.php
@@ -1,11 +1,13 @@
 <?php
+	
+	session_start();
 
 	 require_once('db.class.php');
 
 	$usuario = $_POST['usuario'];
 	$senha = $_POST['senha'];
 
-	$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
+	$sql = "SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
 
 	$objDb = new Db();
     $link = $objDb->conecta_mysql();
@@ -13,9 +15,16 @@
     $resultado_id = mysqli_query($link, $sql);
 
     if ($resultado_id) {
+
     	$dados_usuario = mysqli_fetch_array($resultado_id);
+
 		if(isset($dados_usuario['usuario'])){
-            echo 'usuário existe';
+
+            $_SESSION['usuario'] = $dados_usuario['usuario'];
+            $_SESSION['email'] = $dados_usuario['email'];
+
+            header('Location: home.php');
+
         }else{
             header('Location: index.php?erro=1');
         }
