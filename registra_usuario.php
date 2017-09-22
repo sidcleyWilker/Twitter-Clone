4c41e9eed58b33d0de606116b272c6bd1949d430
sidcleywilker <sidcleywilker@gmail.com>
2017-09-22 02:33:58 -0300

    Utilizando a criptografia md5 para a senha do usuario

--- registra_usuario.php
+++ registra_usuario.php
@@ -4,7 +4,7 @@
 
     $usuario = $_POST['usuario'];
     $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $senha = md5($_POST['senha']);
 
     $objDb = new Db();
     $link = $objDb->conecta_mysql();
