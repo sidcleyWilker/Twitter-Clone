a36e7f67f79f45532c18c1cf1dd247b8f9043332
sidcleywilker <sidcleywilker@gmail.com>
2017-09-21 23:53:39 -0300

    Validando os camuos do form de login com Jquery, so para perder mais tempo, e para fazer uso da tecnologia kkk

--- index.php
+++ index.php
@@ -15,7 +15,30 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	
 		<script>
-			// código javascript						
+			$(document).ready( function(){
+
+				//verificar se os campos de usuário e senha foram devidamente preenchidos
+				$('#btn_login').click(function(){
+
+					var campo_vazio = false;
+
+					if($('#campo_usuario').val() == ''){
+						$('#campo_usuario').css({'border-color': '#A94442'});
+						campo_vazio = true;
+					} else {
+						$('#campo_usuario').css({'border-color': '#CCC'});
+					}
+
+					if($('#campo_senha').val() == ''){
+						$('#campo_senha').css({'border-color': '#A94442'});
+						campo_vazio = true;
+					} else {
+						$('#campo_senha').css({'border-color': '#CCC'});
+					}
+
+					if(campo_vazio) return false;
+				});
+			});						
 		</script>
 	</head>
 
