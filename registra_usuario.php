4d24e68a48d7e59fbcf583a12a7a2eef705f8213
sidcleywilker <sidcleywilker@gmail.com>
2017-09-22 22:28:13 -0300

    Bloquear a inclusao de usuarios ja cadastrado, arte 1

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,6 +9,39 @@
     $objDb = new Db();
     $link = $objDb->conecta_mysql();
 
+    //verificar se o usuário já existe
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' ";
+    if($resultado_id = mysqli_query($link, $sql)){
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['usuario'])){
+            echo 'Usuário já cadastrado';
+        }else{
+            echo 'Usuário não cadastrado, ok, pode cadastrar';        
+        }
+
+    }else{
+        echo 'Erro ao tentar localizar o registro de usuário';
+    }
+
+    //verificar se o e-mail já existe
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' ";
+    if($resultado_id = mysqli_query($link, $sql)){
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['email'])){
+            echo 'Email já cadastrado';
+        }else{
+            echo 'Email não cadastrado, ok, pode cadastrar';
+        }
+    }else{
+        echo 'Erro ao tentar localizar o registro de email';
+    }
+
+    die();
+
     $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
 
     //executar a query
