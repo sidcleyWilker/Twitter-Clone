f68f8da7a8d8b328162d423750a41e8750560039
sidcleywilker <sidcleywilker@gmail.com>
2017-10-02 22:50:33 -0300

    Montando o painel de tweets e seguidores

--- home.php
+++ home.php
@@ -4,6 +4,35 @@
     if(!isset($_SESSION['usuario'])){
         header('Location: index.php?erro=1');
     }
+
+    require_once('db.class.php');
+
+	$objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+	$id_usuario = $_SESSION['id_usuario'];
+
+	//--qtde de tweets
+	$sql = "SELECT COUNT(*) AS qtde_tweets FROM tweet WHERE id_usuario = $id_usuario";
+	$resultado_id = mysqli_query($link, $sql);
+	$qtde_tweets = 0;
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_tweets = $registro['qtde_tweets'];
+	}else{
+		echo 'Erro ao executar a query';
+	}
+
+	//--qtde de seguidores
+	$sql = "SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores WHERE seguindo_id_usuario = $id_usuario";
+	$resultado_id = mysqli_query($link, $sql);
+	$qtde_seguidores = 0;
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_seguidores = $registro['qtde_seguidores'];
+	}else{
+		echo 'Erro ao executar a query';
+	}
 ?>
 
 <!DOCTYPE HTML>
@@ -89,10 +118,10 @@
 	    				<h4><?= $_SESSION['usuario'] ?></h4>
 	    				<hr>
 	    				<div class="col-md-6">
-	    					Twitts <br> 1
+	    					Twitts <br> <?= $qtde_tweets ?>
 	    				</div>
 	    				<div class="col-md-6">
-	    					Seguidores <br> 1
+	    					Seguidores <br> <?= $qtde_seguidores ?>
 	    				</div>
 	    			</div>
 	    		</div>
--- procurar_pessoas.php
+++ procurar_pessoas.php
@@ -1,8 +1,37 @@
-<?
+<?php
 	session_start();
 
 	if(!$_SESSION['usuario']){
 		header('Location: index.php?erro=1');
+	}
+
+	require_once('db.class.php');
+
+	$objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+	$id_usuario = $_SESSION['id_usuario'];
+
+	//--qtde de tweets
+	$sql = "SELECT COUNT(*) AS qtde_tweets FROM tweet WHERE id_usuario = $id_usuario";
+	$resultado_id = mysqli_query($link, $sql);
+	$qtde_tweets = 0;
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_tweets = $registro['qtde_tweets'];
+	}else{
+		echo 'Erro ao executar a query';
+	}
+
+	//--qtde de seguidores
+	$sql = "SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores WHERE seguindo_id_usuario = $id_usuario";
+	$resultado_id = mysqli_query($link, $sql);
+	$qtde_seguidores = 0;
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_seguidores = $registro['qtde_seguidores'];
+	}else{
+		echo 'Erro ao executar a query';
 	}
 ?>
 
@@ -113,10 +142,10 @@
 						<h4><?= $_SESSION['usuario'] ?></h4>
 						<hr />
 						<div class="col-md-6">
-							TWEETS <br /> 1
+							TWEETS <br /> <?= $qtde_tweets ?>
 						</div>
 						<div class="col-md-6">
-							SEGUIDORES <br /> 1
+							SEGUIDORES <br /> <?= $qtde_seguidores ?>
 						</div>
 					</div>
 				</div>
